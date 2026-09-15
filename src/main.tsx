fabbc27f5a499108b634864f20e8666c36179869
lalit-ld43 <looser7970@gmail.com>
2026-09-16 01:02:49 +0530

    fix: set network id at app root to fix scoped transaction crash

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { NetworkId, setNetworkId } from "@midnight-ntwrk/midnight-js-network-id";
 import App from "./App";
 import "./index.css";
+
+// Configure Midnight Network ID
+setNetworkId(NetworkId.TestNet);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

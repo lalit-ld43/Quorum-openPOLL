093cb137c5c740ef0325d3ca76e55b84b91c9b50
lalit-ld43 <looser7970@gmail.com>
2026-09-16 01:06:13 +0530

    fix: pass NetworkId as a string literal instead of using the type alias as a value

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { NetworkId, setNetworkId } from "@midnight-ntwrk/midnight-js-network-id";
+import { setNetworkId } from "@midnight-ntwrk/midnight-js-network-id";
 import App from "./App";
 import "./index.css";
 
 // Configure Midnight Network ID
-setNetworkId(NetworkId.TestNet);
+setNetworkId("TestNet");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
